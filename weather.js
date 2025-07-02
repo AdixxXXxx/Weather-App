9bb75fa9558d1483260c38db5a44e3839664307e
AdixxXXxx <aditya.salwan06@gmail.com>
2025-07-02 17:05:37 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,45 @@
+//http://api.weatherapi.com/v1/current.json?key=10fec1863164473f9be184955250706&q=Noida&aqi=no
+
+const tempfield=document.querySelector("#temp");
+const locName=document.querySelector("#place");
+const condition=document.querySelector("#condition");
+const time=document.querySelector("#ddd");
+const searchbar=document.querySelector("#searchbox input");
+const button=document.querySelector("#bttn button");
+
+ button.addEventListener("click",function(){
+  target=searchbar.value.trim();
+  if(target!==""){
+  fetchResult(target);}
+  else alert("You have not entered the location");
+})
+
+let target= "Noida";
+const fetchResult = async(target)=>{
+
+    let url=`http://api.weatherapi.com/v1/current.json?key=10fec1863164473f9be184955250706&q=${target}&aqi=no`;
+   const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    locName.innerText = data.location.name;
+    tempfield.innerText = data.current.temp_c + " °C";
+    condition.innerText = data.current.condition.text;
+    time.innerText = data.location.localtime;
+}
+fetchResult(target)
+
+const heading=document.querySelector("#title");
+heading.addEventListener("mouseover",function(){
+    heading.style.color="yellow";
+})
+heading.addEventListener("mouseout",function(){
+    heading.style.color="white";
+})
+button.addEventListener("mouseover",function(){
+    button.style.color="white";
+    button.style.backgroundColor="black";
+})
+button.addEventListener("mouseout",function(){
+    button.style.color="black";
+    button.style.backgroundColor="white";
+})
